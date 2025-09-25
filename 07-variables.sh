c7f16ded70df5eb37e1b8dac72e4edad18c9610a
sanjay kumar <palankisanjay@gmail.com>
2025-09-25 20:52:57 +0530

    some message

--- 07-variables.sh
+++ 07-variables.sh
@@ -7,6 +7,6 @@ START_TIME=$(date +%s)
 sleep 10 &
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$($SEND_TIME-$START_TIME)
+TOTAL_TIME=$(($SEND_TIME-$START_TIME))
 
 echo "Scripts executed in: $TOTAL_TIME Seconds"
